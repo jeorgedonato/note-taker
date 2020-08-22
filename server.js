6672544a3348be31a841884206c6efb73f3ac51d
Jeorge Donato <jeorgekhenr@gmail.com>
2020-08-21 20:58:08 -0700

    Modified the server js to make promises to read and write files

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
-
-
+const express = require("express");
+const path = require("path");
+const fs = require('fs');
+const uniqid = require('uniqid');
+const util = require('util');
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
+//GET Request
+//Show the index HTML
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 });
@@ -21,16 +28,41 @@ app.get('/notes', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
-app.post('/notes', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+app.get('/api/notes', async function (req, res) {
+  try {
+    const db = await readFileAsync('db/db.json', 'utf8');
+    res.json(JSON.parse(db));
+  } catch (error) {
+    console.log(err);
+  }
 });
 
-app.delete('/notes/:id', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+app.post('/api/notes', async function (req, res) {
+  try {
+    const db = await readFileAsync('db/db.json', 'utf8');
+    let dbParse = JSON.parse(db);
+    const newNote = req.body;
+    newNote.id = uniqid();
+    dbParse.push(newNote);
+    await writeFileAsync('db/db.json', JSON.stringify(dbParse));
+    res.json(true)
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-
-
+app.delete('/api/notes/:id', async function (req, res) {
+  try {
+    const db = await readFileAsync('db/db.json', 'utf8');
+    const id = req.params.id;
+    let dbParsed = JSON.parse(db);
+    const filtered = dbParsed.filter(note => note.id !== id);
+    await writeFileAsync('db/db.json', JSON.stringify(filtered));
+    res.json(true);
+  } catch (error) {
+    console.log(error)
+  }
+});
 
 // Starts the server to begin listening
 // =============================================================
